c30f43b0220d0bcfdaff98ce7853c0641c18e117
dinaraios <dinara87@yahoo.com>
2025-03-18 18:21:00 +0500

    begining

---
+++ Git_TrainingByMe2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Git_TrainingByMe2
+//
+//  Created by Dinara Ishanova on 18.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
